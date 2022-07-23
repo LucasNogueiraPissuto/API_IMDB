3f1d3b2771597723e683af0df203b75d1818ec25
Lucas Pissuto <lucasnogueirapissuto@gamil.com>
2022-07-23 01:23:12 -0300

    Personalizando texto

--- src/api_imdb/GeradoraDeFigurinhas.java
+++ src/api_imdb/GeradoraDeFigurinhas.java
@@ -1,15 +1,19 @@
 package api_imdb;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
     
     
-    public void Cria() throws IOException{
+    public void Cria(InputStream inputStream, String nomeArquivo) throws IOException{
         BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
         
         int largura = imagemOriginal.getWidth();
@@ -20,13 +24,14 @@ public class GeradoraDeFigurinhas {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
         
-        ImageIO.write(novaImagem, "png", new File("saida/filme"));
-        
-        novaImagem.getGraphics();
-    }
-    
-    public static void main(String[] args) throws IOException{
-        var geradora = new GeradoraDeFigurinhas();
-        geradora.Cria();
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 100, novaAltura - 100);
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
     }
 }
